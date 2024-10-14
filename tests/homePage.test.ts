051ab8670314e6ba1589c4cb3b7a18a7c3b232c6
Serhii Yasenev <serhii.yasenev@gmail.com>
2024-10-14 16:49:18 +0300

    update CI

---
+++ tests/homePage.test.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('check Home page headers', async ({ page }) => {
+  await page.goto('https://serhiiyasenev.github.io/react-trial-project/');
+  await expect(page.getByPlaceholder('Search')).toBeVisible();
+  await expect(page.locator('.nickname')).toHaveText('Serhii');
+  await expect(page.getByText('Serhii Yasenev')).toBeVisible();
+});
